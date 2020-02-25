73a506cd21c6aaf8f0f5b8e0be663cf3615a9691
nhoclove <nguyenvanbac12a2@gmail.com>
2020-02-25 09:30:22 -0500

    pass ingredients from recipe service to shopping list service via a service

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,8 +11,12 @@ export class RecipeDetailComponent implements OnInit {
 
   @Input() recipe: Recipe;
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+  }
+
+  onAddIngredients() {
+    this.recipeService.addIngredients(this.recipe.ingredients);
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,37 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is simply a test', 'https://belleofthekitchen.com/wp-content/uploads/2017/04/oreo-four-layer-dessert-square.jpg'),
-        new Recipe('A next test recipe', 'This is the next test recipe', 'https://realfood.tesco.com/media/images/RFO-1400x919-classic-chocolate-mousse-69ef9c9c-5bfb-4750-80e1-31aafbd80821-0-1400x919.jpg')
+        new Recipe('A test recipe',
+            'This is simply a test',
+            'https://belleofthekitchen.com/wp-content/uploads/2017/04/oreo-four-layer-dessert-square.jpg',
+            [
+                new Ingredient('meat', 1),
+                new Ingredient('buns', 2)
+            ]),
+        new Recipe('A next test recipe',
+            'This is the next test recipe',
+            'https://realfood.tesco.com/media/images/RFO-1400x919-classic-chocolate-mousse-69ef9c9c-5bfb-4750-80e1-31aafbd80821-0-1400x919.jpg',
+            [
+                new Ingredient('meat', 1),
+                new Ingredient('chilli', 1)
+            ])
     ]
+
+    constructor(private shoppingLisrService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredients(ingredients: Ingredient[]) {
+        this.shoppingLisrService.addIngredients(ingredients);
     }
 }
