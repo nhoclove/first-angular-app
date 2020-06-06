907e159d86025115b2ed7946aa3784ca7fad70fb
nguyenvanbac <nguyenvanbac12a2@gmail.com>
2020-06-06 22:54:15 +0700

    send login request with firebase

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,7 +6,7 @@ import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 import { error } from 'protractor';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
@@ -27,6 +27,28 @@ export class AuthService {
                 returnSecureToken: true
             }
         ).pipe(catchError(errorRes => {
+            let errorMessage = 'An unknown error occurred!';
+            if (!errorRes.error || !errorRes.error.error) {
+                return throwError(errorMessage);
+            }
+
+            switch (errorRes.error.error.message) {
+                case 'EMAIL_EXISTS':
+                errorMessage = 'This email exists already';
+            }
+
+            return throwError(errorMessage);
+        }));
+    }
+
+    signin(email: string, password: string) {
+        return this.http
+        .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + AppSettings.FIREBASE_KEY,
+        {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }).pipe(catchError(errorRes => {
             let errorMessage = 'An unknown error occurred!';
             if (!errorRes.error || !errorRes.error.error) {
                 return throwError(errorMessage);
