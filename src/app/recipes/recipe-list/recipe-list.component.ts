76efdcee2c6f2b073d1422e13d976f556d10e140
nguyenvanbac <nguyenvanbac12a2@gmail.com>
2020-06-22 21:16:32 +0700

    add recipes to global store with ngRx

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,35 +1,38 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 import { Recipe } from '../recipe.model';
-import { RecipeService } from '../recipe.service';
+import { AppState } from '../../store/app.reducer';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit, OnDestroy {
-
   subscription: Subscription;
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService,
-              private router: Router,
-              private route: ActivatedRoute) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private store: Store<AppState>
+  ) {}
 
   ngOnInit(): void {
-    this.subscription = this.recipeService.recipeChanged.subscribe(
-      (recipes: Recipe[]) => {
+    this.subscription = this.store
+      .select('recipes')
+      .pipe(map((state) => state.recipes))
+      .subscribe((recipes: Recipe[]) => {
         this.recipes = recipes;
-      }
-    )
-    this.recipes = this.recipeService.getRecipes();
+      });
   }
 
   onNewRecipe() {
-    this.router.navigate(['new'], {relativeTo: this.route})
+    this.router.navigate(['new'], { relativeTo: this.route });
   }
 
   ngOnDestroy() {
--- src/app/shared/data-storage-service.ts
+++ src/app/shared/data-storage-service.ts
@@ -1,39 +1,47 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { map, take, exhaustMap } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { AppSettings } from '../app-settings';
-import { AuthService } from '../auth/auth.service';
+import { AppState } from '../shopping-list/store/shopping-list.reducer';
+import * as RecipeActions from '../recipes/store/recipe.actions';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService, private authService: AuthService) {}
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService,
+    private store: Store<AppState>
+  ) {}
 
-    storeRecipes() {
-        const recipes = this.recipeService.getRecipes();
-        this.http.put(
-            AppSettings.FIREBASE_PROJECT_URL + '/recipes.json',
-            recipes,
-        ).subscribe(
-            response => {
-                console.log(response);
-            }
-        );
-    }
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(AppSettings.FIREBASE_PROJECT_URL + '/recipes.json', recipes)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 
-    fetchRecipes() {
-        return this.http.get<Recipe[]>(
-            AppSettings.FIREBASE_PROJECT_URL + '/recipes.json',
-        ).pipe(
-            map(recipes => {
-                return recipes.map(recipe => {
-                    return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
-                });
-            })).subscribe(recipes => {
-                this.recipeService.setRecipes(recipes);
-            }
-        );
-    }
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(AppSettings.FIREBASE_PROJECT_URL + '/recipes.json')
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
+      .subscribe((recipes) => {
+        // this.recipeService.setRecipes(recipes);
+        this.store.dispatch(new RecipeActions.SetRecipes(recipes));
+      });
+  }
 }
