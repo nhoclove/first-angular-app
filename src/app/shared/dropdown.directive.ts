e3b1df3043c283d7e490cba569b4c3543868b4a3
nguyenvanbac <nguyenvanbac12a2@gmail.com>
2020-06-24 20:43:12 +0700

    clean up ngRx section

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -11,9 +11,10 @@ import {
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
-    this.isOpen = this.elRef.nativeElement.contains(event.target)
+    const a = this.elRef.nativeElement.contains(event.target)
       ? !this.isOpen
       : false;
+    this.isOpen = a;
   }
 
   constructor(private elRef: ElementRef) {}
